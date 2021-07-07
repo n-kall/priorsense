f7bd278096f0a6368f3840c8317cbfed6449737e
n-kall <n-kall@github.com>
2021-07-07 17:35:55 +0300

    disable cmdstanr tests for now

--- tests/testthat/test_moment_matching.R
+++ tests/testthat/test_moment_matching.R
@@ -2,8 +2,8 @@ set.seed(123)
 normal_example <- example_powerscale_model("univariate_normal")
 normal_example_sfit <- rstan::stan(model_code = normal_example$model_code, data = normal_example$data, refresh = FALSE, seed = 123)
 
-normal_example_cmdmodel <- cmdstanr::cmdstan_model(cmdstanr::write_stan_file(normal_example$model_code))
-normal_example_cmdfit <- normal_example_cmdmodel$sample(data = normal_example$data, seed = 123, refresh = 0)
+## normal_example_cmdmodel <- cmdstanr::cmdstan_model(cmdstanr::write_stan_file(normal_example$model_code))
+## normal_example_cmdfit <- normal_example_cmdmodel$sample(data = normal_example$data, seed = 123, refresh = 0)
 
 test_that("moment matching is applied when specified and pareto-k is higher than threshold",
 {  
@@ -49,14 +49,14 @@ test_that("moment matching warnings are displayed",
     ),
     "Moment-matching only works with PSIS. Falling back to moment_match = FALSE"
   )
-  expect_warning(
-    powerscale(
-      fit = normal_example_cmdfit,
-      alpha = 0.2,
-      component = "likelihood",
-      moment_match = TRUE,
-      ),
-    "Moment-matching does not yet work with fits created with cmdstanr. Falling back to moment_match = FALSE"
-  )
+  ## expect_warning(
+  ##   powerscale(
+  ##     fit = normal_example_cmdfit,
+  ##     alpha = 0.2,
+  ##     component = "likelihood",
+  ##     moment_match = TRUE,
+  ##     ),
+  ##   "Moment-matching does not yet work with fits created with cmdstanr. Falling back to moment_match = FALSE"
+  ## )
 }
 )
