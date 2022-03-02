3ba0ed6d9555a61ab9bafca01dbf0fed2d27da7f
n-kall <n-kall@github.com>
2022-03-02 13:57:23 +0200

    add create_powerscaling_data methods for stanfit and CmdStanFit

--- R/create_powerscaling_data.R
+++ R/create_powerscaling_data.R
@@ -1,9 +1,18 @@
+
+
 ##' @rdname powerscale-overview
 ##' @export
-create_powerscaling_data <- function(x, log_prior_fn,
+create_powerscaling_data <- function(x, ...) {
+  UseMethod("create_powerscaling_data")
+
+}
+
+##' @rdname powerscale-overview
+##' @export
+create_powerscaling_data.default <- function(x, log_prior_fn,
                                      joint_log_lik_fn, get_draws,
-                                     unconstrain_pars, log_prob_upars,
-                                     log_ratio_upars) {
+                                     unconstrain_pars = NULL, log_prob_upars = NULL,
+                                     log_ratio_upars = NULL) {
 
   psd <- list(
     fit = x,
@@ -18,4 +27,35 @@ create_powerscaling_data <- function(x, log_prior_fn,
   class(psd) <- c("powerscaling_data", class(psd))
 
   return(psd)
+}
+
+##' @rdname powerscale-overview
+##' @export
+create_powerscaling_data.stanfit <- function(x, ...) {
+
+  create_powerscaling_data.default(
+    x = x,
+    log_prior_fn = log_prior_stanfit,
+    joint_log_lik_fn = joint_log_lik_stanfit,
+    get_draws = get_draws_stanfit,
+    unconstrain_pars = unconstrain_pars,
+    log_prob_upars = log_prob_upars,
+    log_ratio_upars = log_ratio_upars,
+    ...
+  )
+
+}
+
+##' @rdname powerscale-overview
+##' @export
+create_powerscaling_data.CmdStanFit <- function(x, ...) {
+
+  create_powerscaling_data.default(
+    x = x,
+    log_prior_fn = log_prior_CmdStanFit,
+    joint_log_lik_fn = joint_log_lik_CmdStanFit,
+    get_draws = get_draws_CmdStanFit,
+    ...
+  )
+
 }
