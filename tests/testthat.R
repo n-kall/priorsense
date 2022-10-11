75a02cb8d093833310a7a5aa4c7f20e2accfb235
Andrew Johnson <andrew.johnson@arjohnsonau.com>
2022-10-11 11:32:57 +0300

    Test skip fix?

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,6 @@ library(priorsense)
 library(rstan)
 #library(cmdstanr)
 
-if (!identical(Sys.getenv("NOT_CRAN"), "true") && .Platform$OS.type == "windows") {
+if (!identical(Sys.getenv("NOT_CRAN"), "true") && !(.Platform$OS.type == "windows")) {
   test_check("priorsense")
 }
