46e3149c3228825593aa89e3517fa7203134962c
n-kall <n-kall@github.com>
2024-04-23 12:04:20 +0300

    deprecate arguments, rename functions, add options

--- man-roxygen/plot_args.R
+++ man-roxygen/plot_args.R
@@ -2,12 +2,15 @@
 ##'   for which `powerscale_sequence` will first be run on.
 ##' @param variable A character vector of variable names. If `NULL`
 ##'   (the default) all variables will be plotted.
-##' @param quantities A character vector specifying quantities to
-##'   plot. Options are "mean", "median", "sd", "mad", "quantile".
+##' @param quantity A character vector specifying one or several
+##'   quantities to plot. Options are "mean", "median", "sd", "mad",
+##'   "quantile".
 ##' @param quantity_args Named list of further arguments passed to
 ##'   quantity functions. Passed as `.args` to
 ##'   `[posterior::summarise_draws]`.
 ##' @param mcse Boolean; If TRUE will plot +/- 2 * Monte Carlo
 ##'   standard error of the base quantity on the quantities plot.
+##' @param variables (deprecated) Use variable instead.
+##' @param quantities (deprecated) Use quantity instead.
 ##' @param ... Arguments passed to `powerscale_sequence` if `x` is not
 ##'   of class `powerscaled_sequence`.
