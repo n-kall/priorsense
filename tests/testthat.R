bf1665f4307bde3eff63b7b48c6374e285ffe9c4
n-kall <n-kall@github.com>
2024-04-24 11:44:02 +0300

    run tests on windows too

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,5 @@
 library(testthat)
 library(priorsense)
-library(rstan)
-#library(cmdstanr)
 
-if (identical(Sys.getenv("NOT_CRAN"), "true") && !(.Platform$OS.type == "windows")) {
-  test_check("priorsense")
-}
+test_check("priorsense")
+
