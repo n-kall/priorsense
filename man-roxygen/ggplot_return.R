42cee0a83965f1f2d3ba12ec5a2f4ea161cb481a
n-kall <33577035+n-kall@users.noreply.github.com>
2025-03-26 23:21:45 +0200

    if there is only one page in a plot, return just the plot

--- man-roxygen/ggplot_return.R
+++ man-roxygen/ggplot_return.R
@@ -1,1 +1,3 @@
-##' @return A ggplot object that can be further customized using the **ggplot2** package.
+##' @return A ggplot object (or a list of ggplot objects if there us
+##'   more than one page) that can be further customized using the
+##'   **ggplot2** package.
