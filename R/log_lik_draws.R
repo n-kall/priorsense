a5db8466646c4dd66b4b0a8c055818ba8cb4292d
Noa Kallioinen <33577035+n-kall@users.noreply.github.com>
2026-04-21 15:53:17 +0300

    typofix

--- R/log_lik_draws.R
+++ R/log_lik_draws.R
@@ -29,7 +29,7 @@ log_lik_draws.stanfit <- function(x, joint = FALSE,
                                   log_lik_name = "log_lik", ...) {
   
   stan_vars <- names(x)
-  log_lik_name <- stan_vars[grepl(pattern = paste0("^",log_lik_name), stan_vars)]
+  log_lik_name <- stan_vars[grepl(pattern = paste0("^", log_lik_name), stan_vars)]
   log_lik <- as.array(x, pars = log_lik_name)
 
   log_lik <- posterior::as_draws_array(log_lik)
@@ -49,7 +49,7 @@ log_lik_draws.CmdStanFit <- function(x, joint = FALSE,
 
   stan_vars <- x$variables()
   stan_vars <- c(stan_vars$parameters, stan_vars$generated_quantities)
-  log_lik_name <- stan_vars[grepl(pattern = paste0("^",log_lik_name), stan_vars)]
+  log_lik_name <- stan_vars[grepl(pattern = paste0("^", log_lik_name), stan_vars)]
   log_lik <- x$draws(variables = log_lik_name)
 
   if (joint) {
@@ -65,7 +65,7 @@ log_lik_draws.CmdStanFit <- function(x, joint = FALSE,
 log_lik_draws.draws <- function(x, joint = FALSE,
                                 log_lik_name = "log_lik", ...) {
 
-  log_lik <- posterior::subset_draws(x, variable = paste0("^"log_lik_name), regex = TRUE)
+  log_lik <- posterior::subset_draws(x, variable = paste0("^", log_lik_name), regex = TRUE)
 
   if (joint) {
     log_lik <- rowsums_draws(log_lik)
