56b1fd55725fcce8a733ee18686a042f176997cb
Kallioinen Noa <33577035+n-kall@users.noreply.github.com>
2026-08-02 23:54:12 +0300

    formatting

--- R/log_prior_draws.R
+++ R/log_prior_draws.R
@@ -20,93 +20,93 @@
 ##'
 ##' @export
 log_prior_draws <- function(x, ...) {
-  UseMethod("log_prior_draws")
+    UseMethod("log_prior_draws")
 }
 
 
 ##' @rdname log_prior_draws
 ##' @export
 log_prior_draws.stanfit <- function(
-  x,
-  joint = FALSE,
-  log_prior_name = "lprior",
-  ...
+    x,
+    joint = FALSE,
+    log_prior_name = "lprior",
+    ...
 ) {
-  if (!inherits(x, "stanfit")) {
-    stop("Not a stanfit object.", call. = FALSE)
-  }
-  if (x@mode != 0) {
-    stop("Stan model does not contain posterior draws.", call. = FALSE)
-  }
-  if (!requireNamespace("rstan", quietly = TRUE)) {
-    stop("Please load the 'rstan' package.", call. = FALSE)
-  }
+    if (!inherits(x, "stanfit")) {
+        stop("Not a stanfit object.", call. = FALSE)
+    }
+    if (x@mode != 0) {
+        stop("Stan model does not contain posterior draws.", call. = FALSE)
+    }
+    if (!requireNamespace("rstan", quietly = TRUE)) {
+        stop("Please load the 'rstan' package.", call. = FALSE)
+    }
 
-  checkmate::assert_logical(joint, len = 1)
-  checkmate::assert_character(log_prior_name, len = 1)
+    checkmate::assert_logical(joint, len = 1)
+    checkmate::assert_character(log_prior_name)
 
-  log_prior <- posterior::subset_draws(
-    posterior::as_draws_array(x),
-    variable = paste0("^", log_prior_name),
-    regex = TRUE
-  )
+    log_prior <- posterior::subset_draws(
+        posterior::as_draws_array(x),
+        variable = paste0("^", log_prior_name),
+        regex = TRUE
+    )
 
-  if (joint) {
-    log_prior <- rowsums_draws(log_prior)
-    posterior::variables(log_prior) <- log_prior_name
-  }
+    if (joint) {
+        log_prior <- rowsums_draws(log_prior)
+        posterior::variables(log_prior) <- log_prior_name
+    }
 
-  return(log_prior)
+    return(log_prior)
 }
 
 ##' @rdname log_prior_draws
 ##' @export
 log_prior_draws.CmdStanFit <- function(
-  x,
-  joint = FALSE,
-  log_prior_name = "lprior",
-  ...
+    x,
+    joint = FALSE,
+    log_prior_name = "lprior",
+    ...
 ) {
-  checkmate::assert_logical(joint, len = 1)
-  checkmate::assert_character(log_prior_name, len = 1)
+    checkmate::assert_logical(joint, len = 1)
+    checkmate::assert_character(log_prior_name, len = 1)
 
-  all_draws <- x$draws()
+    all_draws <- x$draws()
 
-  log_prior <- posterior::subset_draws(
-    all_draws,
-    variable = paste0("^", log_prior_name),
-    regex = TRUE
-  )
+    log_prior <- posterior::subset_draws(
+        all_draws,
+        variable = paste0("^", log_prior_name),
+        regex = TRUE
+    )
 
-  if (joint) {
-    log_prior <- rowsums_draws(log_prior)
-    posterior::variables(log_prior) <- log_prior_name
-  }
+    if (joint) {
+        log_prior <- rowsums_draws(log_prior)
+        posterior::variables(log_prior) <- log_prior_name
+    }
 
-  return(log_prior)
+    return(log_prior)
 }
 
 ##' @rdname log_prior_draws
 ##' @export
 log_prior_draws.draws <- function(
-  x,
-  joint = FALSE,
-  log_prior_name = "lprior",
-  ...
-) {
-  checkmate::assert_logical(joint, len = 1)
-  checkmate::assert_character(log_prior_name, len = 1)
-
-  log_prior <- posterior::subset_draws(
     x,
-    variable = paste0("^", log_prior_name),
-    regex = TRUE
-  )
+    joint = FALSE,
+    log_prior_name = "lprior",
+    ...
+) {
+    checkmate::assert_logical(joint, len = 1)
+    checkmate::assert_character(log_prior_name)
 
-  if (joint) {
-    log_prior <- rowsums_draws(log_prior)
-    posterior::variables(log_prior) <- log_prior_name
-  }
+    log_prior <- posterior::subset_draws(
+        x,
+        variable = paste0("^", log_prior_name),
+        regex = TRUE
+    )
 
-  return(log_prior)
+    if (joint) {
+        log_prior <- rowsums_draws(log_prior)
+        posterior::variables(log_prior) <- log_prior_name
+    }
+
+    return(log_prior)
 }
